887731bafaaac616627f11cf18a9bcf723e21c1f
Adriano Sanick Padilha <adriano.sanick@gmail.com>
2019-07-30 15:56:35 -0300

    Detecção de face

---
+++ parte9/face.py
@@ -0,0 +1,28 @@
+#Bibliotecas 
+import cv2
+
+#Cassificadores de Haar
+face_cascade = cv2.CascadeClassifier('C:/opencv/build/etc/haarcascades/haarcascade_frontalface_default.xml')
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, img = cap.read()
+
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv2.rectangle(img, (x,y), (x+w, y+h), (255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+
+    cv2.imshow('Face detectada', img)
+
+    k = cv2.waitKey(30) & 0xff
+    if k == 27:
+        break;
+
+cap.release()
+cv2.destroyAllWindows()
